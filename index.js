167b58cfe6c4ddff20b76691c31e82d56ae28b96
June Park / 박준 <35623589+JunePark2018@users.noreply.github.com>
2023-05-26 23:34:54 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// Click Buttons...
-
-$("button").mousedown( function() {
-    $("button").addClass("button-pressed");
-    let x = Math.floor(Math.random() * canvas.width);
-    let y = Math.floor(Math.random() * canvas.height);
-    atoms.push(DrawAtom(x, y, 1));
-});
-
-$("button").mouseup( function() {
-    $("button").removeClass("button-pressed");
-});
-
-
 // Canvas Ready...
 
 let canvas = document.querySelector("canvas");
@@ -19,27 +5,60 @@ let ctx = canvas.getContext("2d");
 
 let atoms = [];
 
-function DrawAtom(x, y, n) {
+function DrawAtom(atom) {
+
+    let {x, y, n, r} = atom;
+
+    // Photon & Neutron
     ctx.beginPath();
 
-    let r = n * 2 + 30;
     ctx.arc(x, y, r, 0, 2*Math.PI, true);
     ctx.lineWidth = 10;
     ctx.stroke();
 
-    ctx.fillStyle = '#aaa';
+    ctx.fillStyle = 'pink';
     ctx.fill();
 
     ctx.closePath();
 
-    return {x, y, n, r};
+    // Electron
+    for (let i = 0; i < n; i++) {
+        ctx.beginPath();
+        if (i < 2)
+            ctx.arc(x + (r + 40) * Math.cos(i * Math.PI), y + (r + 40) * Math.sin(i * Math.PI), 10, 0, 2*Math.PI, true);
+        else if (i < 10)
+            ctx.arc(x + (r + 80) * Math.cos((i - 2) * Math.PI / 4), y + (r + 80) * Math.sin((i - 2) * Math.PI / 4), 10, 0, 2*Math.PI, true);
+        else if (i < 18)
+            ctx.arc(x + (r + 120) * Math.cos((i - 10) * Math.PI / 4), y + (r + 120) * Math.sin((i - 10) * Math.PI / 4), 10, 0, 2*Math.PI, true);
+        else
+            ctx.arc(x + (r + 160) * Math.cos((i - 18) * Math.PI), y + (r + 160) * Math.sin((i - 18) * Math.PI), 10, 0, 2*Math.PI, true);
+        ctx.lineWidth = 10;
+        ctx.stroke();
+        ctx.fillStyle = 'skyblue';
+        ctx.fill();
+        ctx.closePath();
+    }
 }
+
+
+// When user clicks the button...
+
+$(".controls > button").mousedown( function() {
+    $(".controls > button").addClass("button-pressed");
+    let x = Math.floor(Math.random() * canvas.width);
+    let y = Math.floor(Math.random() * canvas.height);
+    let n = $(".controls > input").val();
+    let len = atoms.push({x, y, n, r: n * 2 + 30});
+    DrawAtom(atoms[len - 1]);
+});
 
 
 // When user clicks canvas...
 
 let isCanvasClicked = false;
 let clickedAtom = undefined;
+let canvasX = 0;
+let canvasY = 0;
 
 $("canvas").mousedown( function (event) {
     // Check if the user clicked one of the atoms...
@@ -70,11 +89,24 @@ $("canvas").mousemove( function (event) {
         atoms[clickedAtom.i].y = mouseY + clickedAtom.distY;
         ctx.clearRect(0, 0, canvas.width, canvas.height);        
         for (let atom of atoms)
-            DrawAtom(atom.x, atom.y, atom.n);
-    }    
+            DrawAtom(atom);
+    }
+    else if (isCanvasClicked) {
+        // Move canvas
+    } 
 });
 
-$("canvas").mouseup( function () {
+
+// When the mouse click is done...
+
+$(document).mouseup( function () {
+    $(".controls > button").removeClass("button-pressed");
     clickedAtom = undefined;
     isCanvasClicked = false;
 });
+
+
+// How the object moves...
+function Move(obj, x, y) {
+    $(obj).animate()
+}
